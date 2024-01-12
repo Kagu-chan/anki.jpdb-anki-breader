8bb724e867944a0b9e88e308c5e04a075ccb4946
Kagu-chan <k.boese@kagu-chan.de>
2024-01-12 18:28:06 +0100

    remove unused types

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -4,12 +4,3 @@ declare module '*.scss' {
   const content: string;
   export default content;
 }
-
-declare module '*.svg' {
-  const content: any;
-  export default content;
-}
-declare module '*.png' {
-  const content: any;
-  export default content;
-}
