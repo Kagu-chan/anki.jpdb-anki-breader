e8f678cd86979d2dbe190c4dd2ed09b7c14daffd
Kagu-chan <k.boese@kagu-chan.de>
2024-03-23 21:18:59 +0100

    revert angular changes

--- src/chrome/apps/jpdb-settings.js
+++
@@ -1,6 +0,0 @@
-import { registerListener } from '@lib/messaging';
-
-registerListener('copy-api-key', () => {
-  return Array.from(document.querySelectorAll('td')).find((node) => node.innerText === 'API key')
-    .nextSibling.innerText;
-});
